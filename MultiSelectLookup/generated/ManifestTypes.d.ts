27d116959ea8d99528fd504523efffb0ffd9cf30
VNETIX <liladhar.g@vnetix.com>
2024-12-10 16:21:41 +0530

    MultiSelect Lookup Push

---
+++ MultiSelectLookup/generated/ManifestTypes.d.ts
@@ -0,0 +1,17 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    selectedItems: ComponentFramework.PropertyTypes.LookupProperty;
+    hiddenField: ComponentFramework.PropertyTypes.StringProperty;
+    LookupEntityLogicalName: ComponentFramework.PropertyTypes.StringProperty;
+    NameFieldLogicalName: ComponentFramework.PropertyTypes.StringProperty;
+    LookupOption1LogicalName: ComponentFramework.PropertyTypes.StringProperty;
+    LookupOption2LogicalName: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    selectedItems?: ComponentFramework.LookupValue[];
+    hiddenField?: string;
+}
